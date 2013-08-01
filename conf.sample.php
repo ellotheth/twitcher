1567b1bee2873d8fcfa1084e93ddd67ebf9a17db
ellotheth <ellotheth@gmail.com>
2013-08-01 12:00:32 -0600

    explain how included_circles works in sample

--- conf.sample.php
+++ conf.sample.php
@@ -1,6 +1,8 @@
 <?php
 
-/* Edit this file with your Twitch and Google information */
+/* Edit this file with your Twitch and Google information, and save it as
+ * conf.php
+ */
 
 /* Twitch.tv username to search for new videos */
 $twitch_user = '<twitch username>'
@@ -16,10 +18,17 @@ $gmail_pass = '<google password>';
 /* Setup here: http://bamajr.com/2013/02/16/posting-to-google-plus-via-email/ */
 $gplus_email = '<gvoice #>.33669.<unique key>@txt.voice.google.com';
 
-/* Leave empty to share with 'Your Circles', or add each circle individually */
+/* Circles to include in the broadcast
+ *
+ * Your circles:       $included_circles = array();
+ * Public:             $included_circles = array('Public');
+ * Gamers and Friends: $included_circles = array('Gamers', 'Friends');
+ *
+ * etc.
+ */
 $included_circles = array();
 
-/* Shows up in the 'Received' email header */
+/* Shows up in the 'Received' email header instead of 'localhost' */
 $localhost = '<your server>';
 
 /* Path to log file */
